e05bd2a3f6e3e25dc9df666cad33f91b1c4e7c39
Yuri Diniz <dinizyuri88@gmail.com>
2026-09-23 11:24:26 -0300

    Calculator using functions, switch and if else

--- calculator1.c
+++ calculator1.c
@@ -1,42 +1,67 @@
 #include <stdio.h>
 
-int somar(float a, float b){
+float somar(float a, float b){
     return a + b;
 }
 
-int subtrair(float a, float b){
+float subtrair(float a, float b){
     return a - b;
 }
 
-int multiplicar(float a, float b){
+float multiplicar(float a, float b){
     return a * b;
 }
 
-int dividir(float a, float b){
+float dividir(float a, float b){
     return a / b;
 }
+
+//Para que a calculadora consiga calcular os números decimais, é necessário colocar as variáveis da função como float!
 
 int main(){
 			
     float a, b;
+    char operador;
 
     printf("Digite um número para a: ");
     scanf("%f", &a);
 
+    printf("Digite a operação (+, -, * ou /): ");
+    scanf(" %c", &operador);
+
     printf("Digite um número para b: ");
     scanf("%f", &b);
 
-    float resultado1 = somar(a, b);
-    printf("O resultado da soma é: %.1f\n", resultado1);
+    switch(operador)
+    {
+        case '+':
+            float resultado1 = somar(a, b);
+            printf("O resultado da soma é: %.2f\n", resultado1);
+            break;
+        
+        case '-':
+            float resultado2 = subtrair(a, b);
+            printf("O resultado da subtração é: %.2f\n", resultado2);
+            break;
 
-    float resultado2 = subtrair(a, b);
-    printf("O resultado da subtração é: %.1f\n", resultado2);
+        case '*':
+            float resultado3 = multiplicar(a, b);
+            printf("O resultado da multiplicação é: %.2f\n", resultado3);
+            break;
 
-    float resultado3 = multiplicar(a, b);
-    printf("O resultado da multiplicação é: %.1f\n", resultado3);
-
-    float resultado4 = dividir(a, b);
-    printf("O resultado da divisão é: %.1f\n", resultado4);
+        case '/':
+            if(b != 0){
+                float resultado4 = dividir(a, b);
+                printf("O resultado da divisão é: %.2f\n", resultado4);
+            }else{
+                printf("ERROR 333: Divisão por zero!\n");
+            }
+            break;
+        
+        default:
+            printf("Impossível prosseguirmos!\n");
+            break;
+    }
 
 	return 0;
 }	
